5834b1e71c2a7ec9797095b2c331236af1d362fd
Bumi Denpasar <159143878+BumiDenpasar@users.noreply.github.com>
2024-09-04 07:30:57 +0700

    BUMI DENPASAR

---
+++ MODUL 1- JAVA KELAS 12 BUMI DENPASAR/src/modul1/Modul1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul1;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Modul1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        tugas1 modul1 = new tugas1();
+        modul1.setVisible(true);
+    }
+    
+}
---
+++ MODUL 2 - JAVA KELAS 12 BUMI DENPASAR/src/modul2/Modul2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul2;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Modul2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        tugas2 modul2 = new tugas2();
+        modul2.setVisible(true);
+    }
+    
+}
